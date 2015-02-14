7dad3c89ae51ab2ee16a4a62defd42f694347571
Adriana & Philipp Blume <pair+alcastaneda.pgblu@devbootcamp.com>
2015-02-14 14:49:11 -0600

    recursiveget is working

--- app/models/custom_crawler.rb
+++ app/models/custom_crawler.rb
@@ -5,21 +5,16 @@ class CustomCrawler
 
   def recursive_get(base_url, depth = 0)
     unless depth < 0
-      f = @cw.get(base_url)
-      f[:body].force_encoding('iso-8859-1').encode('utf-8')
-      unless Page.find_by(base_url: base_url)
-        Page.create(base_url: base_url, body: f[:body])
+      begin
+        f = @cw.get(base_url)
+      rescue
+        puts "a connection was refused, move on"
+      end
+      if f && f[:status_code] == 200
+        f[:body].force_encoding('iso-8859-1').encode('utf-8')
+        Page.create(base_url: base_url, body: f[:body]) unless Page.find_by(base_url: base_url)
         f[:links][:links].each { |link| recursive_get(link, depth-1) }
       end
     end
   end
-
-  #   def recursive_get(base_url, depth = 0)
-  #   unless depth < 0
-  #     f = @cw.get(base_url)
-  #     Page.create(base_url: base_url, body: f[:body])
-  #     f[:links][:links].each { |link| recursive_get(link, depth-1) }
-  #   end
-  # end
-
 end
