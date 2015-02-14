c7cf75935977360c4eecf3223e6072837dfbee1c
Adriana & Philipp Blume <pair+alcastaneda.pgblu@devbootcamp.com>
2015-02-14 13:08:51 -0600

    recursive-get works

--- app/models/custom_crawler.rb
+++ app/models/custom_crawler.rb
@@ -3,10 +3,15 @@ class CustomCrawler
     @cw = Cobweb.new
   end
 
-  def recursive_get(base_url)
+  def recursive_get(base_url, depth = 0)
+    unless depth < 0
       f = @cw.get(base_url)
-      Page.create(base_url: base_url, body: f[:body])
-      f[:links][:links].each { |link| recursive_get(link, depth-1) }
+      f[:body].force_encoding('iso-8859-1').encode('utf-8')
+      unless Page.find_by(base_url: base_url)
+        Page.create(base_url: base_url, body: f[:body])
+        f[:links][:links].each { |link| recursive_get(link, depth-1) }
+      end
+    end
   end
 
   #   def recursive_get(base_url, depth = 0)
