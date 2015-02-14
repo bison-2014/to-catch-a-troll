16720312816a8e87bf935f8d8eacf1a4eb91a1a9
Philipp Blume <pgbluster@gmail.com>
2015-02-13 20:51:04 -0600

    uncommented line in travis.yml file

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
 =begin
   resources :tasks
   root to: 'tasks#new'
-  mount Resque:: Server => "/resque"
+  mount Resque::Server => "/resque"
 =end
   # Example resource route with options:
   #   resources :products do
