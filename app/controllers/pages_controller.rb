5fc404a43c2691d4f9d87d28122cd9eb3af9d4c4
Dalal Alrayes <dalrayes@gmail.com>
2015-02-15 12:23:09 -0600

    updated Pages controller with search method and added search to routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,16 @@
 class PagesController < ApplicationController
+
 before_action :authenticate_user!
+
+  def search
+    if params[:search].blank?
+      flash[:notice] = "Please enter valid keywords"
+      render 'search'
+    end
+    @usersearch = Page.search do
+      fulltext params[:search]
+    end
+    @results_found = @usersearch.results
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  
+
   devise_scope :user do
     authenticated :user do
       root 'home#index', as: :authenticated_root
@@ -18,8 +18,9 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  
+
   resources :scrapers, only: [:index, :new, :create]
+  get '/search' => 'pages#search'
   resources :pages, :only => [:index, :new, :show, :create, :update, :delete]
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
