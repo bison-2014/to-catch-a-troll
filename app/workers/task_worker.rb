e320484df718b8b2f069f3b32c2a226d8bacad4e
soborok <soborok@gmail.com>
2015-02-14 18:19:27 -0600

    Scraper MVP working

--- app/workers/task_worker.rb
+++ app/workers/task_worker.rb
@@ -1,13 +1,9 @@
 class TaskWorker
   @queue = :task_queue
 
-  def self.perform(inquiry_id)
-    cobweb = Cobweb.new
-    inquiry = Inquiry.find(inquiry_id)
-    page = cobweb.get(:url)
-    Page.create(title: page.title, body: page[:body])
-    file = File.open("output.html", "wb") { |f| f.write(page.body) }
-    inquiry.update_attributes(response: File.absolute_path(file))
+  def self.perform(page_url)
+    custom_crawler = CustomCrawler.new
+    custom_crawler.recursive_get(page_url)
   end
 
 end
