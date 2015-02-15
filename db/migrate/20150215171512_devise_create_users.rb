450e0b5448c9e0758beb9aaaa216395273e633a8
Dalal Alrayes <dalrayes@gmail.com>
2015-02-15 15:27:05 -0600

    Fixed typos on user migration page

--- db/migrate/20150215171512_devise_create_users.rb
+++ db/migrate/20150215171512_devise_create_users.rb
@@ -35,7 +35,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
     end
 
     add_index :users, :email,                unique: true
-    add_index :DeviseCreateUserss, :reset_password_token, unique: true
+    add_index :users, :reset_password_token, unique: true
     # add_index :models, :confirmation_token,   unique: true
     # add_index :models, :unlock_token,         unique: true
   end
