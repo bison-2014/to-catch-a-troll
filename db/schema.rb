0e2a137671c4a2c8b20a35db0a1f18b6a35b57b2
Adriana C <adriana.lcs316@gmail.com>
2015-02-15 14:36:06 -0600

    drop db to pass travis

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150215171512) do
+ActiveRecord::Schema.define(version: 20150215182409) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20150215171512) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "checksum"
   end
 
   create_table "scrapers", force: :cascade do |t|
