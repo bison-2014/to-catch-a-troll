7eaeb1479f053b56d063bdea1aa0f744ca799750
Dalal Alrayes <dalrayes@gmail.com>
2015-02-15 16:50:26 -0600

    Updated settings for solr

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,18 +1,18 @@
 class PagesController < ApplicationController
 
-before_action :authenticate_user!
+# before_action :authenticate_user!
 
   def search
     if params[:search].blank?
       flash[:notice] = "Please enter valid keywords"
       render 'search'
     end
-    @usersearch = Page.search do
-      fulltext params[:search] do
-        # highlight :body
-      end
-    end
-    @results_found = @usersearch.hits
+    # @usersearch = Page.search do
+    #   fulltext params[:search] do
+    #     # highlight :body
+    #   end
+    # end
+    # @results_found = @usersearch.hits
   end
 
 end
