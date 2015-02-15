fd39412dce05600168c6f5d20185297c0d559cdb
Adriana C <adriana.lcs316@gmail.com>
2015-02-15 12:57:54 -0600

    added devise routes and migration

--- db/migrate/20150215171512_devise_create_users.rb
+++ db/migrate/20150215171512_devise_create_users.rb
@@ -20,10 +20,10 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.inet     :last_sign_in_ip
 
       ## Confirmable
-      # t.string   :confirmation_token
-      # t.datetime :confirmed_at
-      # t.datetime :confirmation_sent_at
-      # t.string   :unconfirmed_email # Only if using reconfirmable
+      t.string   :confirmation_token
+      t.datetime :confirmed_at
+      t.datetime :confirmation_sent_at
+      t.string   :unconfirmed_email # Only if using reconfirmable
 
       ## Lockable
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
@@ -34,8 +34,8 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.timestamps
     end
 
-    add_index :models, :email,                unique: true
-    add_index :models, :reset_password_token, unique: true
+    add_index :users, :email,                unique: true
+    add_index :DeviseCreateUserss, :reset_password_token, unique: true
     # add_index :models, :confirmation_token,   unique: true
     # add_index :models, :unlock_token,         unique: true
   end
