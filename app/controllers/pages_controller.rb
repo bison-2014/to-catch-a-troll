b5972b61bb2110f017971158a1cf53e40985f5fb
Dalal Alrayes <dalrayes@gmail.com>
2015-02-16 17:17:40 -0600

    search integrated with user search query

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,9 @@ class PagesController < ApplicationController
 # before_action :authenticate_user!
 
   def search
+    user = current_user
+    params[:search] = user.search_query.search_query if user.search_query
+
     if params[:search].blank?
       render 'search'
     end
