7659b84ad65e6a91f058f274ca75d5b176bd37e8
soborok <soborok@gmail.com>
2015-02-17 15:46:04 -0600

    using the default config from redisttogo

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,7 +1,3 @@
 
 uri = URI.parse(ENV["REDISTOGO_URL"])
-REDIS = Redis.new(:url => ENV['REDISTOGO_URL'])
-# REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-Resque.redis = REDIS
-
-raise ENV['REDISTOGO_URL']
+Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :thread_safe => true)
