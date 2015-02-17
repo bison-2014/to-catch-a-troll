6f0b1e1ba9c0b9e8febd011870ff5520d7175485
soborok <soborok@gmail.com>
2015-02-16 21:41:51 -0600

    added solr production path, hostname

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,8 +1,20 @@
 <<<<<<< HEAD
 
 uri = URI.parse(ENV["REDISTOGO_URL"])
+<<<<<<< HEAD
+<<<<<<< HEAD
 Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :thread_safe => true)
 =======
 uri = URI.parse(ENV["REDISTOGO_URL"])
 REDIS = Redis.new(:url => ENV['REDISTOGO_URL'])
 >>>>>>> e67445f... added redis addon to heroku
+=======
+# REDIS = Redis.new(:url => ENV['REDISTOGO_URL'])
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+Resque.redis = REDIS
+>>>>>>> e879898... changed redis setting in redis.rb
+=======
+REDIS = Redis.new(:url => ENV['REDISTOGO_URL'])
+# REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+Resque.redis = REDIS
+>>>>>>> 52e911c... set heroku config REDIS_PROVIDER
--- config/initializers/resque_auth.rb
+++ config/initializers/resque_auth.rb
@@ -1,9 +1,13 @@
 Resque::Server.use(Rack::Auth::Basic) do |user, password|
   password = ENV['SECRET']
 <<<<<<< HEAD
+<<<<<<< HEAD
 end
 =======
 end
 
 Resque.redis = REDIS
 >>>>>>> 67a7a77... added Resque.redis = REDIS to resque_auth.rb
+=======
+end
+>>>>>>> fcaa6fa... resolved conflict of Procfile
