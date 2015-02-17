53c5c119e7175d6734b042e9f26589d1c544d7f0
soborok <soborok@gmail.com>
2015-02-16 21:55:03 -0600

    changed redis setting in redis.rb

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,4 @@
 uri = URI.parse(ENV["REDISTOGO_URL"])
-REDIS = Redis.new(:url => ENV['REDISTOGO_URL'])
+# REDIS = Redis.new(:url => ENV['REDISTOGO_URL'])
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+Resque.redis = REDIS
