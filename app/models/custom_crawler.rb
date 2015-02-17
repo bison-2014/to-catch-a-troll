019ad1cdf5cbba988fb017b3e34229a6f0098f7c
Philipp Blume <pgbluster@gmail.com>
2015-02-17 13:46:29 -0600

    custom_crawler model clean

--- app/models/custom_crawler.rb
+++ app/models/custom_crawler.rb
@@ -21,10 +21,11 @@ class CustomCrawler
       if page = Page.find_by(base_url: base_url)
         page.destroy
       end
-      if f && f[:status_code] == 200 && ![".jpg", ".png", ".gif"].any? {|extension| f[:base_url].include? (extension)}
+      if f
+        && f[:status_code] == 200
+        && ![".jpg", ".png", ".gif"].any? {|extension| f[:base_url].include? (extension)}
         f[:body].force_encoding('iso-8859-1').encode('utf-8')
-        sanitized_file = Sanitize.document(f[:body],
-          options)
+        sanitized_file = Sanitize.document(f[:body], options_hash)
         Page.create(base_url: base_url, body: sanitized_file, target_id: target.id)
         f[:links][:links].each { |link| recursive_get(link, depth-1) }
       end
