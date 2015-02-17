383c40a6201af65cee6578adf92f94ed2a6c24dc
Philipp Blume <pgbluster@gmail.com>
2015-02-17 13:44:57 -0600

    git hell

--- app/models/custom_crawler.rb
+++ app/models/custom_crawler.rb
@@ -22,13 +22,21 @@ class CustomCrawler
 >>>>>>> 19607c2... models changes to incorporate target table logic in custom_crawler & user
 =======
   def recursive_get(base_url, depth = 2)
+<<<<<<< HEAD
     options = if (target = Target.find_by(base_url: base_url))
       eval(target.options)
     else
       elements: ["html", "div", "p", "span"]
     end
 >>>>>>> a5ad763... crawler has target
+=======
+>>>>>>> 75d47a9... custom_crawler model clean
     unless depth < 0
+      if (target = Target.find_by(base_url: base_url))
+        options = eval(target.sanitize_options)
+      else
+        options = { elements: ["html", "div", "p", "span"] }
+      end
       begin
         f = @cw.get(base_url)
       rescue => e
@@ -55,15 +63,29 @@ class CustomCrawler
       if page = Page.find_by(base_url: base_url)
         page.destroy
       end
+<<<<<<< HEAD
       if f && f[:status_code] == 200 && ![".jpg", ".png", ".gif"].any? {|extension| f[:base_url].include? (extension)}
 >>>>>>> a5ad763... crawler has target
+=======
+      if f
+        && f[:status_code] == 200
+<<<<<<< HEAD
+        && ![".jpg", ".png", ".gif"].any? {|extension| f[:base_url].include? (extension)}
+>>>>>>> 019ad1c... custom_crawler model clean
+=======
+        && ![".jpg", ".png", ".gif", ".tiff", ".swf"].any? {|extension| f[:base_url].include? (extension)}
+>>>>>>> 75d47a9... custom_crawler model clean
         f[:body].force_encoding('iso-8859-1').encode('utf-8')
-        sanitized_file = Sanitize.document(f[:body],
-          options)
+        sanitized_file = Sanitize.document(f[:body], options_hash)
         Page.create(base_url: base_url, body: sanitized_file, target_id: target.id)
         f[:links][:links].each { |link| recursive_get(link, depth-1) }
       end
     end
     nil
   end
+<<<<<<< HEAD
 end
+=======
+
+end
+>>>>>>> 75d47a9... custom_crawler model clean
