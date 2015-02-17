4d7654e7a90ae431a2e8a2d75c23022e7b06c4be
Dalal Alrayes <dalrayes@gmail.com>
2015-02-16 19:56:05 -0600

    Updated search_strings method

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,7 +1,11 @@
 class Page < ActiveRecord::Base
+  include ActionView::Helpers::SanitizeHelper
+
   searchable do
-    string :base_url, :stored =>true
-    text :body, :stored =>true
+    string :base_url
+    text :body, :stored =>true do
+      strip_tags(body)
+    end
   end
 #  handle_asynchronously :solr_index
 
