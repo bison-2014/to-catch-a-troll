bafcc018d50f88ebb2109ab8674f907566c86aac
soborok <soborok@gmail.com>
2015-02-17 15:35:13 -0600

    verify redis init is running

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,9 @@
+puts "*" * 80
+puts "REDIS INIT CALLED"
+puts "*" * 80
+
 uri = URI.parse(ENV["REDISTOGO_URL"])
 REDIS = Redis.new(:url => ENV['REDISTOGO_URL'])
 # REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-Resque.redis = REDIS
+
+Resque.redis = ENV['REDISTOGO_URL']
