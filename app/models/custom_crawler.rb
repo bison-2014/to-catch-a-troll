890267830c672b72509286dd74b0b6ec2228680e
Philipp Blume <pgbluster@gmail.com>
2015-02-17 13:43:09 -0600

    merge conflict resolved in app.models/target.rb

--- app/models/custom_crawler.rb
+++ app/models/custom_crawler.rb
@@ -6,7 +6,7 @@ class CustomCrawler
     @cw = Cobweb.new(option_hash)
   end
 
-  def recursive_get(base_url, depth = 2)
+  def recursive_get(base_url, depth = 2, options_hash)
     target = Target.find_by(base_url: base_url)
     unless depth < 0
       begin
@@ -14,11 +14,13 @@ class CustomCrawler
       rescue
         puts "a connection was refused, move on"
       end
-      if f && f[:status_code] == 200 #&& !f.is_image?
+      Page.find_by(base_url: base_url).destroy
+      if f
+        && f[:status_code] == 200
+        && ![".jpg", ".png", ".gif"].any? {|extension| f[:base_url].include? (extension)}
         f[:body].force_encoding('iso-8859-1').encode('utf-8')
-        sanitized_file = Sanitize.document(f[:body], target.options_hash)
-        checksum = Digest::MD5.hexdigest(sanitized_file.to_s)
-        Page.create(base_url: base_url, body: sanitized_file, checksum: checksum) unless Page.find_by(base_url: base_url)
+        sanitized_file = Sanitize.document(f[:body], options_hash)
+        Page.create(base_url: base_url, body: sanitized_file, target_id: target.id)
         f[:links][:links].each { |link| recursive_get(link, depth-1) }
       end
     end
