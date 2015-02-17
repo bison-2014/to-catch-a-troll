5e3e1d4ae6a120eeb4720d3cb8b4df37e586e0eb
Adriana <adriana.lcs316@gmail.com>
2015-02-17 09:22:06 -0600

    added target field and associations to pages

--- app/models/target.rb
+++ app/models/target.rb
@@ -1,3 +1,4 @@
 class Target < ActiveRecord::Base
-  belongs_to: user
+  belongs_to :user
+  has_many :pages
 end
--- db/migrate/20150214205252_create_pages.rb
+++ db/migrate/20150214205252_create_pages.rb
@@ -3,6 +3,7 @@ class CreatePages < ActiveRecord::Migration
     create_table :pages do |t|
       t.string :base_url
       t.text :body
+      t.belongs_to :target
 
       t.timestamps null: false
     end
