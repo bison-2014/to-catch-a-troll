c7749237ba4a0804abfe53690c92089d0861fb4c
Dalal Alrayes <dalrayes@gmail.com>
2015-02-18 11:46:24 -0600

    Updated schema with db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20150218045002) do
-
+ActiveRecord::Schema.define(version: 20150218163415) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,9 +34,9 @@ ActiveRecord::Schema.define(version: 20150218045002) do
   create_table "pages", force: :cascade do |t|
     t.string   "base_url"
     t.text     "body"
+    t.integer  "target_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "checksum"
   end
 
   create_table "scrapers", force: :cascade do |t|
@@ -67,11 +65,6 @@ ActiveRecord::Schema.define(version: 20150218045002) do
     t.integer  "default_depth"
     t.datetime "created_at"
     t.datetime "updated_at"
-  end
-
-  create_table "usermailers", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
