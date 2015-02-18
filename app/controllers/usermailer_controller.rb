59069cfec78bbc170614c945c08a8a38c6ef0666
Adriana C <adriana.lcs316@gmail.com>
2015-02-18 10:38:06 -0600

    customized email messagegit add .git add .!

--- app/controllers/usermailer_controller.rb
+++ app/controllers/usermailer_controller.rb
@@ -1,11 +1,12 @@
 class UsermailerController < ApplicationController
  
   def index
-
+    @user = User.find_by(id: current_user.id)
   end
   
   def create
-    Usermailer.gmail_message.deliver_now
+    @user = User.find_by(id: current_user.id)
+    Usermailer.gmail_message(@user).deliver_now
     flash[:notice]= "Sent"
     redirect_to "/home"
   end
--- app/mailers/usermailer.rb
+++ app/mailers/usermailer.rb
@@ -2,8 +2,11 @@ class Usermailer < ActionMailer::Base
   default from: "patroll.app@gmail.com"
   layout 'mailer'
 
-  def gmail_message
-    mail(to:'adriana.lcs316@gmail.com', subject: 'test email')
+  def gmail_message(user)
+    @user= user
+    mail(to: @user.email, 
+      subject: 'Patroll found something!',
+      content_type: "text/html")
     # subject 'Message via Gmail'
     # recipients 'adriana.lcs316@gmail.com'
     # from 'patroll.app@gmail.com'
