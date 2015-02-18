002c7c58120a32873d1c40bcb2b57d1dc585cd28
Philipp Blume <pgbluster@gmail.com>
2015-02-18 07:05:02 -0600

    extra line feeds removed from crawler model

--- spec/models/custom_crawler_spec.rb
+++ spec/models/custom_crawler_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 # require 'custom_crawler'
 
 describe "CustomCrawler" do
-  @body = <<HEEJIN
+  @body = <<HTML
   <html><div>Foo</div>
   <div><a href="http://www.other%20example.com/troll2">Bar</a></div>
   </html>
-HEEJIN
+HTML
 
   before(:each) do
-    @ccrawler = CustomCrawler.new
+    @ccrawler = CustomCrawler.new(1)
     FakeWeb.register_uri(:any, "http://example.com/troll1", body: @body)
     FakeWeb.register_uri(:any, "http://other%20example.com/troll2", body: "Quxxxx")
   end
@@ -21,13 +21,13 @@ HEEJIN
   end
 
   describe "#recursive_get" do
-    xit "retrieves the body associated with the base url" do
+    it "retrieves the body associated with the base url" do
       @ccrawler.recursive_get("http://example.com/troll1", 0)
       expect (Page.last)[:body].to eq(body)
     end
 
-    xit "retrieves the body of the linked url" do
-      @ccrawler.recursive_get("http://example.com/troll1", 0)
+    it "retrieves the body of the linked url" do
+      @ccrawler.recursive_get("http://example.com/troll1", 1)
       expect (Page.last)[:body].to eq("Quxxxx")
     end
   end
