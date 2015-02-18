1caa20c493c02118983d5b72d3f7ddea74c5d833
Philipp Blume <pgbluster@gmail.com>
2015-02-17 21:31:45 -0600

    custom crawler testing

--- spec/models/custom_crawler_spec.rb
+++ spec/models/custom_crawler_spec.rb
@@ -21,32 +21,15 @@ HTML
   end
 
   describe "#recursive_get" do
-    it "retrieves the body associated with the base url" do
+    xit "retrieves the body associated with the base url" do
       @ccrawler.recursive_get("http://example.com/troll1", 0)
       expect (Page.last)[:body].to eq(body)
     end
 
-    it "retrieves the body of the linked url" do
+    xit "retrieves the body of the linked url" do
       @ccrawler.recursive_get("http://example.com/troll1", 1)
       expect (Page.last)[:body].to eq("Quxxxx")
     end
   end
 
-  # describe "#recursive_get" do
-  #   let (:url)        { "http://www.example.com" }
-  #   let (:cw)         { Cobweb.new }
-  #   let (:body)       { cw.get(url)[:body] }
-  #   let (:ccrawler)   { CustomCrawler.new }
-  #   let (:page_count) { Page.all.count }
-
-  #   it "increases the number of page objects by 1" do
-  #     ccrawler.get(url, 0)
-  #     expect (Page.all.count).to eq(page_count + 1)
-  #   end
-
-  #   it "retrieves the correct body" do
-  #     ccrawler.get(url, 0)
-  #     expect (Page.last[:body]).to eq(body)
-  #   end
-  # end
 end
