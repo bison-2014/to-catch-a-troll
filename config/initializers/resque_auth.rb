b268921667baa35bb11f73ee85f5bf8d21f35a3e
soborok <soborok@gmail.com>
2015-02-18 18:24:50 -0600

    added resque auth with devise

--- config/initializers/resque_auth.rb
+++ config/initializers/resque_auth.rb
@@ -1,3 +1,15 @@
-Resque::Server.use(Rack::Auth::Basic) do |user, password|
-  password = ENV['SECRET']
+class AuthenticatedMiddleware
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+    if env['warden'].authenticated?
+      @app.call(env)
+    else
+      [403, {'Content-Type' => 'text/plain'}, ['Authenticate first']]
+    end
+  end
 end
+
+Resque::Server.use(AuthenticatedMiddleware)
